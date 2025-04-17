60a456c055365ee92917137b5b41368f4b21757b
Amez <carmensasamez@gmail.com>
2025-04-17 15:18:50 +0200

    inyectar clerk en el proyecto (sing-in/sing-up

---
+++ constants/StorageKeys.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StorageKeys = () => {
+  return (
+    <div>StorageKeys</div>
+  )
+}
+
+export default StorageKeys
---
+++ hooks/useBooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useBooks = () => {
+  return (
+    <div>useBooks</div>
+  )
+}
+
+export default useBooks
